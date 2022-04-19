a72cea2d21047df4f3d83642f6a3f35bcd32a9df
kaizawa97 <kai109870@gmail.com>
2022-04-20 00:27:34 +0900

    finish: 0414 homework

--- 0414.c
+++ 0414.c
@@ -10,7 +10,18 @@ int main() {
 	int *f;
 
 	a = 1;
-	b = 11111111;
+	b = 1234567890;
+	c[0] = 10;
+	d = 'A';
+	e[0] = 'H'; e[1]= 'E'; e[2] = 'L'; e[3] = 'L'; e[4] = 'O'; e[5] = '\0';
+	f = &a;
 	
 	printf("a = %d, &a = %p\n",a,&a);
+	printf("b = %ld, &b = %p\n",b,&b);
+	printf("c[0] = %d, &c[0] = %p\n",c[0],&c[0]);
+	printf("d = %d, &d = %p\n",d,&d);
+	printf("e = %s, &e = %p\n",e,&e);
+	printf("&e[0] = %p\n",&e[0]);
+	printf("&e[1] = %p\n",&e[1]);
+	printf("f = %p, &f = %p\n",f,&f);
 }
