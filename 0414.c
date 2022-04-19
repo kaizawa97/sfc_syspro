c688e7dea5ad3336f53cfefbe96ddf599a7c7485
kaizawa97 <kai109870@gmail.com>
2022-04-19 15:30:03 +0900

    new files

---
+++ 0414.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int a;
+	long b;
+	int c[5];
+	char d;
+	char e[5];
+	int *f;
+
+	a = 1;
+	b = 11111111;
+	
+	printf("a = %d, &a = %p\n",a,&a);
+}
